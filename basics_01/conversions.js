481408a92c09defc047d96f51dad39b94fd09ca8
ammysharma11 <108935281+ammysharma11@users.noreply.github.com>
2023-12-19 09:38:58 +0000

    conversions

---
+++ basics_01/conversions.js
@@ -0,0 +1,44 @@
+let score ="ammy"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueINnumber =Number(score)
+//console.log(typeof valueINnumber);
+//console.log(valueINnumber);
+
+// "33"=>33
+// "33an"=> NaN (NotaNumber)
+// true=>1; false =>0
+let isLoggedIn= "ammy";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// ""=>false
+//"ammy"=>true
+
+let x=10;
+let stringno = String(x);
+//console.log(x);
+//console.log(typeof(stringno));
+
+let value=3
+let Negvalue = -value
+console.log(Negvalue);
+
+console.log(2+2)
+console.log(2**3)
+console.log(2%3)
+console.log(2/3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log((3+4)*5%3);
+console.log(+true)
+//console.log(true+)  //gives error not allowed 
+console.log(+"")
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
