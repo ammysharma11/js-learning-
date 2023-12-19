4efd4b31b9a18222a5b02de5f266d9a24172d95b
ammysharma11 <108935281+ammysharma11@users.noreply.github.com>
2023-12-19 10:04:24 +0000

    compairsions

---
+++ basics_01/compairsons.js
@@ -0,0 +1,11 @@
+console.log(null>0);
+console.log(null==0); //== work as equality so it doesn't compare
+console.log(null>=0); // it just compares the value it converts the null to zero
+
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<0);
+
+
+console.log("2"===2)  // ===  does not convert the data type it just compaers if datatype are also same then the value
+ 
