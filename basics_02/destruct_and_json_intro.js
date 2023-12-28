c55fe193d0381932bf815507a9e81427163d695f
ammysharma11 <108935281+ammysharma11@users.noreply.github.com>
2023-12-28 11:23:43 +0000

    destruct&jsonintro

---
+++ basics_02/destruct_and_json_intro.js
@@ -0,0 +1,11 @@
+const course={
+    coursename: "js in hndi",
+    price:"555",
+    courseInstructor:"ammy"
+}
+
+const {courseInstructor:instructor}= course
+//console.log(courseInstructor);
+console.log(instructor)
+
+
