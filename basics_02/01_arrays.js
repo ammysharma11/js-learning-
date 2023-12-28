03185dbb8cc076a68299711d490bfa252e36b723
ammysharma11 <108935281+ammysharma11@users.noreply.github.com>
2023-12-28 11:22:34 +0000

    array

---
+++ basics_02/01_arrays.js
@@ -0,0 +1,42 @@
+const myArr =[ 0,1,2,3,4,5]
+const myheros= ["ammy",'jimmy']
+const myarr2= new Array(1,2,3,4)
+
+console.log(myArr[0]);
+
+//array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(9)
+console.log(myArr)
+myArr.shift()
+console.log(myArr)
+
+// console.log(myArr.includes(9))  // gives boolean answer
+// console.log(myArr.indexOf(3))
+
+// const newArr = myArr.join()  //converts to string 
+
+// console.log(myArr);
+// console.log(newArr);
+
+// //slice, splice
+// console.log("A",myArr);
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+// console.log("B",myArr);
+
+// const myn2= myArr.splice(1,3)
+// console.log("B",myArr); // difference btw slice and splice is that splice makes actual changes in the array like reference 
+// console.log(myn2)
+
+
+
+
+
+
+
