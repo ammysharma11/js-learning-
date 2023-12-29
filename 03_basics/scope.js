0163fe4c501e621f27af36486e006a145622e2b0
ammysharma11 <108935281+ammysharma11@users.noreply.github.com>
2023-12-29 10:14:33 +0000

    scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,45 @@
+function one()
+{
+    const username='ammy'
+
+    function two()
+    {
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website);   
+    two()
+}
+//one();
+//console.log(one());
+if(true)
+{
+    const username = "ammy"
+    if(username=="ammy")
+    {
+        const website="youtube"
+        console.log(username+website);
+
+    }
+    
+}
+
+// +++++++++++++INTERESTING ++++++++++++++
+
+ addone(5);
+
+function addone(num)
+{
+    return num+1;
+
+}
+
+addTwo(5) // when we define function this way we can't  access it before intializtion
+
+const addTwo=function(num)
+{ 
+    return num +2;
+}
+
+
+
